211666c3b2b50686dff3073b768afdd7f9850e61
m3rashid <m3rashid.hussain@gmail.com>
2023-06-27 15:32:14 +0530

    fix(app): init

--- src/components/PenSettings.tsx
+++ src/components/PenSettings.tsx
@@ -1,7 +1,7 @@
-import { Component } from "solid-js";
-import { createSettings } from "../SettingsProvider";
+import { Component } from 'solid-js';
+import { createSettings } from '../SettingsProvider';
 
-import style from "./PenSettings.module.css";
+import style from './PenSettings.module.css';
 
 type RangeEvent = InputEvent & {
   currentTarget: HTMLInputElement;
@@ -20,63 +20,63 @@ const PenSettings: Component = () => {
   };
 
   return (
-    <div class="p-1 z-50 shadow rounded bg-white dark:bg-gray-600 mt-2 text-gray-700 dark:text-gray-200">
-      <div class="w-full">
-        <label class={style.label} for="size">
-          Size
+    <div class='p-4 z-50 shadow rounded-md bg-white dark:bg-gray-600 mt-2 text-gray-700 dark:text-gray-200'>
+      <div class={style.upperDiv}>
+        <label class={style.label} for='size'>
+          SIZE
         </label>
         <input
           class={style.range}
-          type="range"
-          id="size"
-          min="1"
-          max="64"
-          step="0.63"
+          type='range'
+          id='size'
+          min='1'
+          max='64'
+          step='0.63'
           value={settings().size}
           onInput={handleChange}
         />
       </div>
-      <div class="w-full">
-        <label class={style.label} for="thinning">
-          Thinning
+      <div class={style.upperDiv}>
+        <label class={style.label} for='thinning'>
+          THINNING
         </label>
         <input
           class={style.range}
-          type="range"
-          id="thinning"
-          min="-1"
-          max="1"
-          step="0.02"
+          type='range'
+          id='thinning'
+          min='-1'
+          max='1'
+          step='0.02'
           value={settings().thinning}
           onInput={handleChange}
         />
       </div>
-      <div class="w-full">
-        <label class={style.label} for="smoothing">
-          Smoothing
+      <div class={style.upperDiv}>
+        <label class={style.label} for='smoothing'>
+          SMOOTHENING
         </label>
         <input
           class={style.range}
-          type="range"
-          id="smoothing"
-          min="0"
-          max="2"
-          step="0.02"
+          type='range'
+          id='smoothing'
+          min='0'
+          max='2'
+          step='0.02'
           value={settings().smoothing}
           onInput={handleChange}
         />
       </div>
-      <div class="w-full">
-        <label class={style.label} for="streamline">
-          Streamline
+      <div class={style.upperDiv}>
+        <label class={style.label} for='streamline'>
+          STREAMLINE
         </label>
         <input
           class={style.range}
-          type="range"
-          id="streamline"
-          min="0"
-          max="1"
-          step="0.01"
+          type='range'
+          id='streamline'
+          min='0'
+          max='1'
+          step='0.01'
           value={settings().streamline}
           onInput={handleChange}
         />
